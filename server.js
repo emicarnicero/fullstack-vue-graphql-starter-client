e6328703d90050adc7c20d8fee99596e2ebcb8e3
emicarnicero <ecarnicero@qplus.com.ar>
2019-11-07 19:07:19 +0100

    Change history mode

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
+var history = require("connect-history-api-fallback");
+
 app = express();
 app.use(serveStatic(path.join(__dirname, "dist")));
-app.use(
-  history({
-    disableDotRule: true,
-    verbose: true
-  })
-);
+app.use(history());
+
 const port = process.env.PORT || 80;
 app.listen(port);
 
